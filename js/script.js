5c8c34230ad0add7bcc2c92a7a60a92776a315ca
Erin Mahon <erin.mahon14@gmail.com>
2016-01-28 20:42:54 -0500

    commented out general sections of sass style sheets

--- js/script.js
+++ js/script.js
@@ -15,24 +15,6 @@ var autoSlide = setInterval(function() {
     currentIndex = 0;
   }
   cycleItems();
-}, 3000);
-
-$('.next').click(function() {
-  clearInterval(autoSlide);
-  currentIndex += 1;
-  if (currentIndex > itemAmt - 1) {
-    currentIndex = 0;
-  }
-  cycleItems();
-});
-
-$('.prev').click(function() {
-  clearInterval(autoSlide);
-  currentIndex -= 1;
-  if (currentIndex < 0) {
-    currentIndex = itemAmt - 1;
-  }
-  cycleItems();
-});
+}, 2000);
 
 
